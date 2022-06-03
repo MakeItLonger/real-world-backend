2323b66146e9f28e65a15db57a445c54c106bd0b
Vadym Zavadiuk <zavadiuk@gmail.com>
2022-06-03 11:55:20 +0300

    init and set up project

---
+++ app.js
@@ -0,0 +1,81 @@
+const fs = require('fs'),
+    http = require('http'),
+    path = require('path'),
+    methods = require('methods'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    cors = require('cors'),
+    passport = require('passport'),
+    errorhandler = require('errorhandler'),
+    mongoose = require('mongoose');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+
+app.use(cors());
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(require('method-override')());
+app.use(express.static(__dirname + '/public'));
+
+app.use(
+    session({
+        secret: 'conduit',
+        cookie: { maxAge: 60000 },
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+if (!isProduction) {
+    app.use(errorhandler());
+}
+
+if (isProduction) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    mongoose.connect('mongodb://localhost/conduit');
+    mongoose.set('debug', true);
+}
+
+app.use(require('./routes'));
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+if (!isProduction) {
+    app.use((err, req, res, next) => {
+        console.log(err.stack);
+
+        res.status(err.status || 500);
+
+        res.json({
+            errors: {
+                message: err.message,
+                error: err,
+            },
+        });
+    });
+}
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        errors: {
+            message: err.message,
+            error: {},
+        },
+    });
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log('Listening on port ' + server.address().port);
+});
