0ea679a14dd2c74955ab45bfbf6088d051925f74
Vadym Zavadiuk <zavadiuk@gmail.com>
2022-06-10 19:23:28 +0300

    JEC8-190 create queryable endpoints for lists and feed

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -35,6 +35,108 @@ router.param('comment', (req, res, next, id) => {
         .catch(next);
 });
 
+router.get('/', auth.optional, (req, res, next) => {
+    const query = {};
+    const limit = 20;
+    const offset = 0;
+
+    if (typeof req.query.limit !== 'undefined') {
+        limit = req.query.limit;
+    }
+
+    if (typeof req.query.offset !== 'undefined') {
+        offset = req.query.offset;
+    }
+
+    if (typeof req.query.tag !== 'undefined') {
+        query.tagList = { $in: [req.query.tag] };
+    }
+
+    Promise.all([
+        req.query.author ? User.findOne({ username: req.query.author }) : null,
+        req.query.favorited
+            ? User.findOne({ username: req.query.favorited })
+            : null,
+    ])
+        .then((results) => {
+            const author = results[0];
+            const favoriter = results[1];
+
+            if (author) {
+                query.author = author._id;
+            }
+
+            if (favoriter) {
+                query._id = { $in: favoriter.favorites };
+            } else if (req.query.favorited) {
+                query._id = { $in: [] };
+            }
+
+            return Promise.all([
+                Article.find(query)
+                    .limit(Number(limit))
+                    .skip(Number(offset))
+                    .sort({ createdAt: 'desc' })
+                    .populate('author')
+                    .exec(),
+                Article.count(query).exec(),
+                req.payload ? User.findById(req.payload.id) : null,
+            ]).then((results) => {
+                const articles = results[0];
+                const articlesCount = results[1];
+                const user = results[2];
+
+                return res.json({
+                    articles: articles.map((article) => {
+                        return article.toJSONFor(user);
+                    }),
+                    articlesCount: articlesCount,
+                });
+            });
+        })
+        .catch(next);
+});
+
+router.get('/feed', auth.required, (req, res, next) => {
+    const limit = 20;
+    const offset = 0;
+
+    if (typeof req.query.limit !== 'undefined') {
+        limit = req.query.limit;
+    }
+
+    if (typeof req.query.offset !== 'undefined') {
+        offset = req.query.offset;
+    }
+
+    User.findById(req.payload.id).then((user) => {
+        if (!user) {
+            return res.sendStatus(401);
+        }
+
+        Promise.all([
+            Article.find({ author: { $in: user.following } })
+                .limit(Number(limit))
+                .skip(Number(offset))
+                .populate('author')
+                .exec(),
+            Article.count({ author: { $in: user.following } }),
+        ])
+            .then((results) => {
+                const articles = results[0];
+                const articlesCount = results[1];
+
+                return res.json({
+                    articles: articles.map((article) => {
+                        return article.toJSONFor(user);
+                    }),
+                    articlesCount: articlesCount,
+                });
+            })
+            .catch(next);
+    });
+});
+
 router.post('/', auth.required, (req, res, next) => {
     User.findById(req.payload.id)
         .then((user) => {
@@ -68,40 +170,54 @@ router.get('/:article', auth.optional, (req, res, next) => {
 });
 
 router.put('/:article', auth.required, (req, res, next) => {
-    if (req.article._id.toString() === req.payload.id.toString()) {
-        if (typeof req.body.article.title !== 'undefined') {
-            req.article.title = req.body.article.title;
-        }
+    User.findById(req.payload.id).then((user) => {
+        if (req.article.author._id.toString() === req.payload.id.toString()) {
+            if (typeof req.body.article.title !== 'undefined') {
+                req.article.title = req.body.article.title;
+            }
 
-        if (typeof req.body.article.description !== 'undefined') {
-            req.article.description = req.body.article.description;
-        }
+            if (typeof req.body.article.description !== 'undefined') {
+                req.article.description = req.body.article.description;
+            }
 
-        if (typeof req.body.article.body !== 'undefined') {
-            req.article.body = req.body.article.body;
-        }
+            if (typeof req.body.article.body !== 'undefined') {
+                req.article.body = req.body.article.body;
+            }
 
-        req.article
-            .save()
-            .then((article) => {
-                return res.json({ article: article.toJSONFor(user) });
-            })
-            .catch(next);
-    } else {
-        return res.send(403);
-    }
-});
+            if (typeof req.body.article.tagList !== 'undefined') {
+                req.article.tagList = req.body.article.tagList;
+            }
 
-router.delete('/:article', auth.required, (req, res, next) => {
-    User.findById(req.payload.id).then(() => {
-        if (req.article.author.toString() === req.payload.id.toString()) {
-            return req.article.remove().then(() => {
-                return res.sendStatus(204);
-            });
+            req.article
+                .save()
+                .then((article) => {
+                    return res.json({ article: article.toJSONFor(user) });
+                })
+                .catch(next);
         } else {
             return res.sendStatus(403);
         }
     });
+});
+
+router.delete('/:article', auth.required, (req, res, next) => {
+    User.findById(req.payload.id)
+        .then((user) => {
+            if (!user) {
+                return res.sendStatus(401);
+            }
+
+            if (
+                req.article.author._id.toString() === req.payload.id.toString()
+            ) {
+                return req.article.remove().then(() => {
+                    return res.sendStatus(204);
+                });
+            } else {
+                return res.sendStatus(403);
+            }
+        })
+        .catch(next);
 });
 
 router.post('/:article/favorite', auth.required, (req, res, next) => {
