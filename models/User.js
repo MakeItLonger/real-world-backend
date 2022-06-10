195f6bbdda513f6bca0ca1dbd5c29f9e6c25dd71
Vadym Zavadiuk <zavadiuk@gmail.com>
2022-06-10 11:14:15 +0300

    JEC8-188 create folowing functionality

--- models/User.js
+++ models/User.js
@@ -101,4 +101,34 @@ UserSchema.methods.isFavorite = function (id) {
     });
 };
 
+UserSchema.methods.follow = function (id) {
+    if (!this.favorites.includes(id)) {
+        this.following = this.following.concat([id]);
+    }
+
+    return this.save();
+};
+
+UserSchema.methods.unfollow = function (id) {
+    this.following.remove(id);
+    return this.save();
+};
+
+UserSchema.methods.isFollowing = function (id) {
+    return this.following.some((followId) => {
+        return followId.toString() === id.toString();
+    });
+};
+
+UserSchema.methods.toProfileJSONFor = function (user) {
+    return {
+        username: this.username,
+        bio: this.bio,
+        image:
+            this.image ||
+            'https://static.productionready.io/images/smiley-cyrus.jpg',
+        following: user ? user.isFollowing(this._id) : false,
+    };
+};
+
 mongoose.model('User', UserSchema);
